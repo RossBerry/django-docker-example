673ee603ce2baa9a0ec00e83aa2336da6e766f7d
Kenneth Berry <rosskberry@gmail.com>
2019-07-05 20:11:07 -0600

    Increase wait time before database restore

--- website.py
+++ website.py
@@ -6,12 +6,49 @@ Contains command functions for managing a postgres db and django website running
 inside docker containers.
 
 """
-
 import datetime
 import os
 import socket
 import sys
 import time
+
+
+def backup_db():
+    """
+    Backs up the postgres database.
+    """
+    print("Backing up website!")
+    if "postgres_data" not in os.listdir("./"):
+        print("postgres_data directory does not already exist!")
+        print("creating postgres_data directory for db backup storage")
+        send_command("mkdir postgres_data")
+    dt = datetime.datetime.now()
+    db_backup_file = f"dump_{dt.date().day}-{dt.date().month}-{dt.date().year}_{dt.time().hour}_{dt.time().minute}_{dt.time().second}.sql"
+    db_backup_cmd = f"docker exec -t postgres_db pg_dumpall -c -U postgres > ./postgres_data/{db_backup_file}"
+    send_command(db_backup_cmd)
+    print(f"Database saved to {db_backup_file}")
+
+def build():
+    """
+    Starts the website with a new build.
+    """
+    build_cmd = "docker-compose up -d --build"
+    send_command(build_cmd)
+    # wait until postgres database port is available
+    while not check_port(5432):
+        pass
+    time.sleep(10)  # wait for database to finish startup
+    restore_db()
+
+def check_port(port):
+    """
+    Checks if a port is open.
+    """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex(('127.0.0.1', port))
+    port_open = True if result == 0 else False
+    sock.close()
+    return port_open
 
 def create_super_user():
     """
@@ -30,22 +67,6 @@ def get_latest_backup():
         if backups:
             return backups[-1]
 
-def backup_db():
-    """
-    Backs up the postgres database.
-    """
-    print("Backing up website!")
-    if "postgres_data" not in os.listdir("./"):
-        print("postgres_data directory does not already exist!")
-        print("creating postgres_data directory for db backup storage")
-        send_command("mkdir postgres_data")
-    dt = datetime.datetime.now()
-    db_backup_file = f"dump_{dt.date().day}-{dt.date().month}-{dt.date().year}_{dt.time().hour}_{dt.time().minute}_{dt.time().second}.sql"
-    db_backup_cmd = f"docker exec -t postgres_db pg_dumpall -c -U postgres > ./postgres_data/{db_backup_file}"
-    send_command(db_backup_cmd)
-    print(f"Database saved to {db_backup_file}")
-
-
 def restore_db():
     """
     Restores database from latest backup file, if available.
@@ -58,6 +79,21 @@ def restore_db():
     else:
         print("Cannot find a backup file to restore from")
 
+def save_stop():
+    """
+    Backs up the website, then shuts down the database and webserver.
+    """
+    backup_db()
+    stop()
+
+def send_command(command):
+    """
+    Sends a command to the command line.
+    """
+    if PLATFORM == "win" or PLATFORM == "osx":
+        os.system(command)
+    elif PLATFORM == "lnx":
+        os.system(f"sudo {command}")
 
 def start():
     """
@@ -68,17 +104,8 @@ def start():
     # wait until postgres database port is available
     while not check_port(5432):
         pass
-    time.sleep(5)  # wait for database to finish startup
+    time.sleep(10)  # wait for database to finish startup
     restore_db()
-
-
-def save_stop():
-    """
-    Backs up the website, then shuts down the database and webserver.
-    """
-    backup_db()
-    stop()
-
 
 def stop():
     """
@@ -87,39 +114,6 @@ def stop():
     print("Shutting down website!")
     stop_cmd = "docker-compose down"
     send_command(stop_cmd)
-
-
-def build():
-    """
-    Starts the website with a new build.
-    """
-    build_cmd = "docker-compose up -d --build"
-    send_command(build_cmd)
-    # wait until postgres database port is available
-    while not check_port(5432):
-        pass
-    time.sleep(5)  # wait for database to finish startup
-    restore_db()
-
-
-def check_port(port):
-    """
-    Checks if a port is open.
-    """
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex(('127.0.0.1', port))
-    port_open = True if result == 0 else False
-    sock.close()
-    return port_open
-
-def send_command(command):
-    """
-    Sends a command to the command line.
-    """
-    if PLATFORM == "win" or PLATFORM == "osx":
-        os.system(command)
-    elif PLATFORM == "lnx":
-        os.system(f"sudo {command}")
 
 PLATFORM = sys.platform
 if PLATFORM == "linux" or PLATFORM == "linux2":
@@ -133,11 +127,11 @@ if __name__ == "__main__":
     COMMANDS = [
         "backup_db",
         "build",
+        "create_super_user",
         "restore_db",
         "save_stop",
         "start",
-        "stop",
-        "create_super_user"
+        "stop"
     ]
 
     if sys.argv[1] in COMMANDS:
